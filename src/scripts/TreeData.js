51079cd1e3274e96829cb418217fedd6353da6ef
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-01-04 12:11:48 -0800

    Add organize and group functionality

--- src/scripts/TreeData.js
+++ src/scripts/TreeData.js
@@ -1,20 +1,41 @@
 
 class TreeData {
   async getData() {
-    await fetch('https://data.cityofnewyork.us/resource/uvpi-gqnh.json?boroname=Manhattan&')
+    // ORGANIZE BY
+    const organize = document.getElementById('organize-by-select');
+    const selectedOrganize = organize.value;
+    let organizeToRequest = ''
+    if (selectedOrganize === 'staten-island') {
+      organizeToRequest = 'Staten Island';
+    } else {
+      let capitalized = selectedOrganize[0].toUpperCase() + selectedOrganize.slice(1);
+      organizeToRequest = capitalized;
+    }
+    console.log(selectedOrganize);
+
+    // GROUP BY
+    const group = document.getElementById('group-by-select');
+    const selectedGroupBy = group.value;
+    let requestBase = '';
+    let groupByToRequest = '';
+    if (selectedGroupBy === 'species-name') {
+      requestBase = '&spc_common='
+      groupByToRequest = ''
+    }
+    await fetch(`https://data.cityofnewyork.us/resource/uvpi-gqnh.json?boroname=${organizeToRequest}`)
       .then(response => {
         return response.json()
       })
       .then(data => {
         console.log(data);
-        for (let i = 0; i < 10; i++) {
-          if (data[i]) {
-            let treeList = document.querySelector(".tree-list")
-            treeList.appendChild(document.createElement('li')).innerHTML = `${data[i].address}`
-          } else {
-            continue;
-          }
-        }
+        // for (let i = 0; i < 10; i++) {
+        //   if (data[i]) {
+        //     let treeList = document.querySelector(".tree-list")
+        //     treeList.appendChild(document.createElement('li')).innerHTML = `${data[i].address}`
+        //   } else {
+        //     continue;
+        //   }
+        // }
       })
       .catch(error => {
         console.error(error);
