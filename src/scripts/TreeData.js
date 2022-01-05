accbcd1d4c189ded819fe3fbc37f046d20e6d593
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-01-04 17:44:02 -0800

    Add functionality for getSpeciesInfo and refactor getBoroughInfo

--- src/scripts/TreeData.js
+++ src/scripts/TreeData.js
@@ -1,49 +1,46 @@
 
 class TreeData {
   async getData(name) {
-    const SPECIES = ['London planetree', 'honeylocust', 'cherry', 'red maple', 'green ash', 'American elm', 'ginkgo', 'willow oak', 'sugar maple', 'mulberry'];
     let data = await fetch(`https://data.cityofnewyork.us/resource/uvpi-gqnh.json?boroname=${name}`)
     if (data.ok) {
       data = await data.json();
       return data;
     } else {
-      throw new Error('Incorrect');
+      throw new Error('Invalid action');
     }
   }
 
   onClick() {
-    // return this.getData('Manhattan');
+    // return this.getData('');
     return this.getBoroughInfo();
+    // return this.getSpeciesInfo();
   }
 
   getBoroughInfo() {
     const BOROUGHS = ['Manhattan', 'Brooklyn', 'Staten Island', 'Queens', 'Bronx'];
-    let boroughData = {
-      manhattan: [],
-      brooklyn: [],
-      statenIsland: [],
-      queens: [],
-      bronx: []
-    }
+    let boroughData = {}
+    BOROUGHS.forEach(borough => {
+      boroughData[borough] = []
+    })
 
     for (let i = 0; i < BOROUGHS.length; i++) {
-      if (BOROUGHS[i] === 'Manhattan') {
-        this.getData(BOROUGHS[i])
-          .then(result => { boroughData['manhattan'].push(result)})
-          .then(() => console.log(boroughData));
-      } else if (BOROUGHS[i] === 'Brooklyn') {
-        this.getData(BOROUGHS[i])
-          .then(result => { boroughData['brooklyn'].push(result) });
-      } else if (BOROUGHS[i] === 'Staten Island') {
-        this.getData(BOROUGHS[i])
-          .then(result => { boroughData['statenIsland'].push(result) });
-      } else if (BOROUGHS[i] === 'Queens') {
-        this.getData(BOROUGHS[i])
-          .then(result => { boroughData['queens'].push(result) });
-      } else {
-        this.getData(BOROUGHS[i])
-          .then(result => { boroughData['bronx'].push(result) });
-      }
+      this.getData(BOROUGHS[i])
+        .then(result => {boroughData[BOROUGHS[i]].push(result)})
+        .then(() => console.log(boroughData));
+    }
+  }
+
+  getSpeciesInfo() {
+    const SPECIES = ['London planetree', 'honeylocust', 'cherry', 'red maple', 'green ash', 'American elm', 'ginkgo', 'willow oak', 'sugar maple', 'mulberry'];
+    let speciesData = {};
+    SPECIES.forEach(tree => {
+      speciesData[tree] = [];
+    })
+
+    for (let i = 0; i < SPECIES.length; i++) {
+      this.getData(SPECIES[i])
+        .then(result => {speciesData[SPECIES[i]].push(result)})
+        .then(() => console.log(speciesData));
     }
   }
 }
